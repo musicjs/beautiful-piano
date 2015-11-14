bfb530c97fd0e5e9db013bc8bb16b6585ffd14da
Anton Wilhelm <timaschew@gmail.com>
2015-11-15 00:03:49 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Beautiful Piano
+
+This library provides a JavaScript API to generate HTML markup for a
+beautiful musical keyboard. It comes with a CSS file which makes
+
+## Setup
+Either embed the file from the `dist` directory via script tag in your HTML:
+
+```
+<script src="beautiful-piano/dist/piano.js"></script>
+```
+
+Then you can access to the global variable `piano`.
+
+Otherwise install it via npm `npm i beatiful-piano`
+
+Don't forget to include the CSS file, for instance:
+
+```
+<link rel="stylesheet" href="beautiful-piano/styles.css">
+```
+
+## Usage
+
+```
+piano(document.querySelector('body'), {octaves: 3});
+```
+
+## API
+
+### piano(parent, options)
+
+__Arguments:__
+- `parent` - DOM element where to inject the piano
+- `options` Options object with either an `octaves` property or a `range` property.
+You can also pass a `namesMode` property.
+	- `octaves` - A positive integer, the amount of octaves to generate, starts always with an __A__ and ends with a __C__
+	- `range` - Options object with a custm range, h
+		- `startKey` - A string, key where the piano should start
+		- `startOctave` - An integer on which octave the piano should start
+		- `endKey` - A string, key where the piano should end
+		- `endOctave` - An integer on which octave the piano should end
+	- `namesMode` - A string, either `sharp` or `flat` when names for the black keys are shown
+
+
+
+### Demo
+
+See the demo on [rawgit](https://rawgit.com/MusicJS/beatiful-piano/master/demo
+/index.html)
+
+## WTF? I can't hear anything!
+
+This library provides just a visual interface.
+Playing a sound while pressing a key is comming soon as an extra module.
+
+
+## Credits
+All visual credits goes to [Taufik Nurrohman](https://github.com/tovic)
+
+I started with the style sheets from his codepen and modified it for more dynamic
+use cases.
---
+++ index.js
@@ -0,0 +1,99 @@
+var domify = require('domify');
+
+var blackKeyClassMap = {
+    C: 0,
+    D: 1,
+    E: 2,
+    F: 3,
+    G: 4,
+    A: 5,
+    B: 6
+};
+
+var keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+var keyReverseMap = {
+    C: 0,
+    D: 1,
+    E: 2,
+    F: 3,
+    G: 4,
+    A: 5,
+    B: 6
+};
+
+var keysLength = keys.length;
+
+// black keys belongs to the previous white key
+// It's the DOM representation
+// avoid here to use #, because it's not a valid CSS selector
+var blackKeyMap = {
+    C: false,
+    D: ['Cs', 'Db'],
+    E: ['Ds', 'Eb'],
+    F: false,
+    G: ['Fs', 'Gb'],
+    A: ['Gs', 'Ab'],
+    B: ['As', 'Bb']
+};
+
+var convertAccidental = function(keyName) {
+    return keyName.replace('s', '♯').replace('b', '♭');
+}
+
+var octaves = [0,1,2,3,4,5,6,7,8,9,10];
+
+module.exports = function(parent, options) {
+    if (options == null) {options = {}}
+
+    var startKey = 'A';
+    var startOctave = 3;
+    var endKey = 'C'
+    var endOctave = 5;
+    var namesMode = 0; // show sharps as default
+    if (options.octaves != null) {
+        if (options.octaves <= 0) {
+            console.warn('octaves need to be a positive number!');
+        }
+        var o = parseInt(options.octaves);
+        var oHalf = Math.floor(o/2);
+        if (o % 2 === 1) {
+            startOctave = 3 - oHalf;
+            endOctave = 4 + oHalf + 1;
+        } else {
+            startOctave = 3 - oHalf;
+            endOctave = 4 + oHalf;
+        }
+
+    } else if (options.range != null) {
+        startKey = options.range.startKey;
+        startOctave = parseInt(options.range.startOctave);
+        endKey = options.range.endKey;
+        endOctave = parseInt(options.range.endOctave);
+
+    }
+    if (options.namesMode != null) {
+        if (options.namesMode === 'flat')  {
+            namesMode = 1;
+        }
+    }
+    var keyElementArray = [];
+    var firstOccurrence = true;
+    for (var o=startOctave; o<=endOctave; o++) {
+        for (var k=keyReverseMap[startKey]; k<(o === endOctave ? keyReverseMap[endKey]+1 : keysLength); k++) {
+            var n = keys[k]; // key name
+            if (blackKeyMap[n] && !firstOccurrence) {
+                var blackNames = blackKeyMap[n].map(function(k) {return k+o});
+                keyElementArray.push('<li><div data-keyname=' +  n + o + ' class="anchor ' + n + o + '"></div><span data-keyname="' + convertAccidental(blackKeyMap[n][namesMode]) + '" class="' + blackNames.join(' ') + '"></span></li>');
+            } else {
+                keyElementArray.push('<li><div data-keyname=' + n + o + ' class="anchor ' + n + o + '"></div></li>');
+            }
+            if (firstOccurrence) {
+                firstOccurrence = false;
+            }
+        }
+        startKey = 'C'; // continue next octave from C
+    }
+    var pianoWrapper = domify('<ul id="beautiful-piano">\n  ' + keyElementArray.join('\n  ') + '</ul>')
+    parent.appendChild(pianoWrapper);
+}
