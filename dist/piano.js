230d769a9755a0a0d43771349d1c12d63f8baf26
Anton Wilhelm <timaschew@gmail.com>
2015-11-15 01:37:05 +0100

    use simple chars for sharps and flats

--- dist/piano.js
+++ dist/piano.js
@@ -39,7 +39,7 @@ var blackKeyMap = {
 };
 
 var convertAccidental = function(keyName) {
-    return keyName.replace('s', '\u0023').replace('b', '\u266D');
+    return keyName.replace('s', '#');
 }
 
 var octaves = [0,1,2,3,4,5,6,7,8,9,10];
