5293f28e0a8591d4b9ac63fcf364bdffee583deb
Anton Wilhelm <timaschew@gmail.com>
2015-11-15 19:43:49 +0100

    update build output

--- dist/piano.js
+++ dist/piano.js
@@ -1,57 +1,98 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.piano = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var domify = require('domify');
 
-var blackKeyClassMap = {
-    C: 0,
-    D: 1,
-    E: 2,
-    F: 3,
-    G: 4,
-    A: 5,
-    B: 6
+var keys = {
+    en: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    de: ['C', 'D', 'E', 'F', 'G', 'A', 'H']
 };
-
-var keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 var keyReverseMap = {
-    C: 0,
-    D: 1,
-    E: 2,
-    F: 3,
-    G: 4,
-    A: 5,
-    B: 6
+    en: {
+        C: 0,
+        D: 1,
+        E: 2,
+        F: 3,
+        G: 4,
+        A: 5,
+        B: 6
+    },
+    de: {
+        C: 0,
+        D: 1,
+        E: 2,
+        F: 3,
+        G: 4,
+        A: 5,
+        H: 6
+    }
 };
 
-var keysLength = keys.length;
+var keysLength = keys.en.length;
 
 // black keys belongs to the previous white key
 // It's the DOM representation
 // avoid here to use #, because it's not a valid CSS selector
 var blackKeyMap = {
-    C: false,
-    D: ['Cs', 'Db'],
-    E: ['Ds', 'Eb'],
-    F: false,
-    G: ['Fs', 'Gb'],
-    A: ['Gs', 'Ab'],
-    B: ['As', 'Bb']
+    en: {
+        C: false,
+        D: ['&nbsp;C#', '&nbsp;Db'],
+        E: ['&nbsp;D#', '&nbsp;Eb'],
+        F: false,
+        G: ['&nbsp;F#', '&nbsp;Gb'],
+        A: ['&nbsp;G#', '&nbsp;Ab'],
+        B: ['&nbsp;A#', '&nbsp;Bb']
+    },
+    de: {
+        C: false,
+        D: ['Cis', 'Des'],
+        E: ['Dis', '&nbsp;Es'],
+        F: false,
+        G: ['Fis', 'Ges'],
+        A: ['Gis', '&nbsp;As'],
+        B: ['&nbsp;As', '&nbsp;&nbsp;B']
+    }
 };
 
-var convertAccidental = function(keyName) {
-    return keyName.replace('s', '#');
-}
-
+// scientific notation
 var octaves = [0,1,2,3,4,5,6,7,8,9,10];
 
+// https://de.wikipedia.org/wiki/Tonsymbol#Deutsche_Tonbezeichnungen
+var helmholtzNotation = [
+    {index: 2, upper: true},
+    {index: 1, upper: true},
+    {index: null, upper: true},
+    {index: null, upper: false},
+    {index: 1, upper: false},
+    {index: 2, upper: false},
+    {index: 3, upper: false},
+    {index: 4, upper: false},
+    {index: 5, upper: false},
+    {index: 6, upper: false},
+    {index: 7, upper: false},
+]
+
 module.exports = function(parent, options) {
+    var getCurrentNotation = function(key, octaveIndex) {
+        if (notation === 'scientific') {
+            return key + octaveIndex;
+        }
+        var result = helmholtzNotation[octaveIndex];
+        if (result.upper) {
+            return key.toUpperCase() + (result.index ? result.index : '')
+        } else {
+            return key.toLowerCase() + (result.index ? result.index : '')
+        }
+    }
+
     if (options == null) {options = {}}
 
+    var lang = 'en';
     var startKey = 'A';
     var startOctave = 3;
     var endKey = 'C'
     var endOctave = 5;
     var namesMode = 0; // show sharps as default
+    var notation = 'scientific';
     if (options.octaves != null) {
         if (options.octaves <= 0) {
             console.warn('octaves need to be a positive number!');
@@ -65,6 +106,12 @@ module.exports = function(parent, options) {
             startOctave = 3 - oHalf;
             endOctave = 4 + oHalf;
         }
+        while (startOctave < 0) {
+            startOctave++;
+            endOctave++;
+        }
+        console.log('startOctave: ', startOctave);
+        console.log('endOctave: ', endOctave);
 
     } else if (options.range != null) {
         startKey = options.range.startKey;
@@ -73,21 +120,30 @@ module.exports = function(parent, options) {
         endOctave = parseInt(options.range.endOctave);
 
     }
-    if (options.namesMode != null) {
-        if (options.namesMode === 'flat')  {
-            namesMode = 1;
-        }
+
+    if (options.namesMode === 'flat')  {
+        namesMode = 1;
+    }
+
+    if (options.lang === 'de') {
+        lang = 'de';
+    }
+
+    if (options.notation === 'helmholz') {
+        notation = 'helmholz';
     }
     var keyElementArray = [];
     var firstOccurrence = true;
     for (var o=startOctave; o<=endOctave; o++) {
-        for (var k=keyReverseMap[startKey]; k<(o === endOctave ? keyReverseMap[endKey]+1 : keysLength); k++) {
-            var n = keys[k]; // key name
-            if (blackKeyMap[n] && !firstOccurrence) {
-                var blackNames = blackKeyMap[n].map(function(k) {return k+o});
-                keyElementArray.push('<li><div data-keyname=' +  n + o + ' class="anchor ' + n + o + '"></div><span data-keyname="' + convertAccidental(blackKeyMap[n][namesMode]) + '" class="' + blackNames.join(' ') + '"></span></li>');
+        for (var k=keyReverseMap.en[startKey]; k<(o === endOctave ? keyReverseMap.en[endKey]+1 : keysLength); k++) {
+            var n = keys.en[k]; // key name
+            var displayWhiteKey = getCurrentNotation(keys[lang][k], o);
+            if (blackKeyMap.en[n] && !firstOccurrence) {
+                var blackNames = blackKeyMap.en[n].map(function(k) {return k+o});
+                var displayBlackKey = blackKeyMap[lang][n][namesMode];
+                keyElementArray.push('<li><div data-keyname=' +  displayWhiteKey + ' class="anchor ' + n + o + '"></div><span data-keyname="' + displayBlackKey + '" class="' + blackNames.join(' ') + '"></span></li>');
             } else {
-                keyElementArray.push('<li><div data-keyname=' + n + o + ' class="anchor ' + n + o + '"></div></li>');
+                keyElementArray.push('<li><div data-keyname=' + displayWhiteKey + ' class="anchor ' + n + o + '"></div></li>');
             }
             if (firstOccurrence) {
                 firstOccurrence = false;
